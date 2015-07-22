0e733fb3dd5cf97eb8068cc418611f27c49a893c
ron <ron.gregory@pacbell.net>
2015-07-22 19:44:25 +0000

    Cleaned up per code review.

--- ttt.rb
+++ ttt.rb
@@ -4,31 +4,31 @@ require 'pry'
 WINNING_LINES = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
 
 def initialize_board
-  b = {}
-  (1..9).each {|position| b[position] = ' '}
-  b
+  board = {}
+  (1..9).each {|position| board[position] = ' '}
+  board
 end
 
-def draw_board(b)
+def draw_board(board)
   system 'clear'
-  puts " #{b[1]} | #{b[2]} | #{b[3]} "
+  puts " #{board[1]} | #{board[2]} | #{board[3]} "
   puts "---+---+---"
-  puts " #{b[4]} | #{b[5]} | #{b[6]} "
+  puts " #{board[4]} | #{board[5]} | #{board[6]} "
   puts "---+---+---"
-  puts " #{b[7]} | #{b[8]} | #{b[9]} "
+  puts " #{board[7]} | #{board[8]} | #{board[9]} "
 end
 
-def player_picks_square(b)
+def player_picks_square(board)
   loop do
     puts "Pick a square (1 - 9):"
     position = gets.chomp.to_i
-    empties = empty_positions(b)    # get the empty ones
-    if( empties.member?(position) )
-      b[position] = 'X'
+    empties = empty_positions(board)    # get the empty ones
+    if empties.member?(position)
+      board[position] = 'X'
       break
     else
-      draw_board(b)
-      if(position == 0)
+      draw_board(board)
+      if position == 0
         puts "Invalid input."
       else
         puts "That spot is already taken."
@@ -37,63 +37,53 @@ def player_picks_square(b)
   end
 end
 
-def empty_positions(b)
-  b.select {|k, v| v == ' ' }.keys
+def empty_positions(board)
+  board.select {|_k, v| v == ' ' }.keys
 end
 
-def find_two(b, mrkr)
+def find_two_in_a_row(board, marker)
   WINNING_LINES.each do |line|
-    h = Hash[ line.map { |x| [x, b[x]]} ]
-    position = two_in_a_row(h,mrkr)
-    return position if( position ) 
+    row= Hash[line.map{ |a_line| [a_line, board[a_line]]}]
+    position = two_in_a_row(row, marker)
+    return position if position
   end
-  return nil
+  false
 end
 
 # checks to see if two in a row
 # returns true if two mrkr in one row, plus an empty
-def two_in_a_row(hsh, mrkr)
-  if hsh.values.count(mrkr) == 2
-    hsh.select{|k,v| v == ' '}.keys.first
+def two_in_a_row(row, marker)
+  if row.values.count(marker) == 2
+    row.select{|_k,v| v == ' '}.keys.first
   else
     false
   end
 end
 
-def computer_picks_square(b)
-  #strategy to find two in a row
-  #first try to find two x's in a row
-  position = find_two(b, 'O')
-  if(position)
-    # Win if you can first
-    b[position]= 'O'
-    return
-  else
-    puts "now try X"
+def computer_picks_square(board)
+  #first try to find two O's in a row to win
+  position = find_two_in_a_row(board, 'O')
+  if position == false
     #otherwise try to block X
-    position= find_two(b, 'X')
-    if(position)
-      #go for the kill!
-      b[position]= 'O'
-      return
+    position= find_two_in_a_row(board, 'X')
+    if position == false
+      position = empty_positions(board).sample
     end
   end
-  
-  position = empty_positions(b).sample
-  b[position] = 'O'
+  board[position] = 'O'
 end
 
-def check_winner(b)
+def check_winner(board)
   WINNING_LINES.each do |line|
-    if b[line[0]] == 'X' and b[line[1]] == 'X' and b[line[2]] == 'X'
+    if board[line[0]] == 'X' and board[line[1]] == 'X' and board[line[2]] == 'X'
       puts "#{line[0]}, #{line[1]}, #{line[2]}"
       return 'Player'
-    elsif b[line[0]] == 'O' and b[line[1]] == 'O' and b[line[2]] == 'O'
+    elsif board[line[0]] == 'O' and board[line[1]] == 'O' and board[line[2]] == 'O'
       puts "#{line[0]}, #{line[1]}, #{line[2]}"
       return 'Computer'
     end
   end
-  return nil
+  nil
 end
 
 board = initialize_board
